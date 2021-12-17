2c77e05003e266903e9b063b3067bcaf8e357537
Juan Carlos <86580442+JuanLuky@users.noreply.github.com>
2021-12-17 19:57:17 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,102 @@
+// Menu de Navegação
+const element = document.querySelectorAll(".nav-infos-itens li")
+const img = document.querySelectorAll(".nav-infos-itens li img")
+const todosDepartamento = document.querySelector(".nav-link")
+
+for (let i = 0; i < element.length; i++) {
+    element[0].onclick = () => {
+        if (img) {
+            // Removendo as classes
+            removeClass();
+
+            img[0].setAttribute('src', 'assets/icones_home/icones_categorias/som-e-video-vermelho.svg');
+            element[0].classList.add("active");
+        }
+
+    }
+}
+for (let i = 0; i < element.length; i++) {
+    element[1].onclick = () => {
+        if (img) {
+            // Removendo as classes
+            removeClass();
+
+            img[1].setAttribute('src', 'assets/icones_home/icones_categorias/farois-lanternas-vermelho.svg');
+            element[1].classList.add("active");
+        }
+
+    }
+}
+for (let i = 0; i < element.length; i++) {
+    element[2].onclick = () => {
+        if (img) {
+            // Removendo as classes
+            removeClass();
+
+            // Adicionando as classes
+            img[2].setAttribute('src', 'assets/icones_home/icones_categorias/acessorios-internos-vermelho.svg');
+            element[2].classList.add("active");
+        }
+    }
+}
+for (let i = 0; i < element.length; i++) {
+    element[3].onclick = () => {
+        if (img) {
+            // Removendo as classes
+            removeClass();
+
+            img[3].setAttribute('src', 'assets/icones_home/icones_categorias/acessorios-externos-vermelho.svg');
+            element[3].classList.add("active");
+        }
+
+    }
+}
+for (let i = 0; i < element.length; i++) {
+    element[4].onclick = () => {
+        if (img) {
+            // Removendo as classes
+            removeClass();
+
+            // Adicionando as classes
+            img[4].setAttribute('src', 'assets/icones_home/icones_categorias/vestuarios-vermelho.svg');
+            element[4].classList.add("active");
+        }
+    }
+}
+for (let i = 0; i < element.length; i++) {
+    element[5].onclick = () => {
+        if (img) {
+            // Removendo as classes
+            removeClass();
+
+            // Adicionando as classes
+            img[5].setAttribute('src', 'assets/icones_home/icones_categorias/infantil-vermelho.svg');
+            element[5].classList.add("active");
+        }
+    }
+}
+
+todosDepartamento.addEventListener('click', () => {
+    removeClass();
+})
+// Removendo a imagem e cor vermelha
+function removeClass() {
+    img[0].setAttribute('src', 'assets/icones_home/icones_categorias/som-e-video-cinza.svg');
+    element[0].classList.remove("active");
+
+    img[1].setAttribute('src', 'assets/icones_home/icones_categorias/farois-lanternas-cinza.svg');
+    element[1].classList.remove("active");
+
+    img[2].setAttribute('src', 'assets/icones_home/icones_categorias/acessorios-internos-cinza.svg');
+    element[2].classList.remove("active");
+
+    img[3].setAttribute('src', 'assets/icones_home/icones_categorias/acessorios-externos-cinza.svg');
+    element[3].classList.remove("active");
+
+    img[4].setAttribute('src', 'assets/icones_home/icones_categorias/vestuarios-cinza.svg');
+    element[4].classList.remove("active");
+
+    img[5].setAttribute('src', 'assets/icones_home/icones_categorias/infantil-cinza.svg');
+    element[5].classList.remove("active");
+}
 
